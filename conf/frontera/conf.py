11bc78aa3c37a2e3fe577d948ba731016e38c0fc
Yongfei Wang <wangyf@users.noreply.github.com>
2020-07-23 09:27:41 -0700

    Update conf.py

--- conf/frontera/conf.py
+++ conf/frontera/conf.py
@@ -8,7 +8,7 @@ account: jmz
 """
 login = 'login.frontera.tacc.utexas.edu'
 hosts = 'h2ologin3',
-maxcores = 56 
+maxcores = 48 
 maxram = 6553600 
 minnodes = 1
 maxnodes = 8640 
@@ -26,7 +26,8 @@ fortran_flags = {
 #    'O': ('-fast -vec-report -no-ipo -g ',) + _,
 #    'O': ('-O3 -ffast-math -funroll-loops -Wall','-cpp','-fdefault-real-8') + _,
 #     'O': ('-O3 -xCORE-AVX512','-check bounds -check uninit -fpp -Ddoubleprecision=1','-real-size 64') + _,
-    'O': (' -O3 -xCORE-AVX512 -qopt-zmm-usage=high','-fpp -Ddoubleprecision=1','-real-size 64') + _,	
+#    'O': (' -O3 -xCORE-AVX512 -qopt-zmm-usage=high','-fpp -Ddoubleprecision=1','-real-size 64') + _,	
+    'O': (' -O3 -xCORE-AVX512','-fpp -Ddoubleprecision=1','-real-size 64') + _,	
 #    'O': ('-ipo -O3 -no-prec-div -fp-model fast=2 -xHost -xCORE-AVX512','-fpp -Ddoubleprecision=1','-real-size 64') + _,
 #    'O': ('-O2',) + _,
 }
