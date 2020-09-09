cdb51cb4a5425eacea1faa325a81ca2c203218e7
Yongfei Wang <wangyf@users.noreply.github.com>
2020-09-08 17:43:07 -0700

    Update data.py

--- extras/data.py
+++ extras/data.py
@@ -11,6 +11,16 @@ except ImportError:
 
 repo = os.path.expanduser( '~/mapdata' )
 
+
+def readbin(file,shape,inputdtype=None):
+    fd = open( file ,'rb')
+    if inputdtype == None:
+        inputdtype = np.dtype( 'f8' ).str #real*4
+    matrix = np.fromfile(fd, inputdtype).reshape(shape).astype(idtype)
+    print('Read size check',file,matrix.shape)
+    fd.close()
+    return matrix
+
 def tsurf( path ):
     """
     Read GOCAD (http://www.gocad.org) trigulated surface "Tsurf" files.
